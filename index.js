613fc60c9a2f1a7505b3eaa5ef62969dec735f96
yeyuguo <346243440@qq.com>
2018-11-25 00:37:02 +0800

    feat(编写销售域名简单页面，使用 backbone 练习渲染功能):

---
+++ index.js
@@ -0,0 +1,55 @@
+
+
+
+const Model = Backbone.Model.extend({
+    defaults: function () {
+        return {
+            title: '姓名',
+            text: '叶玉国'
+        }
+    }
+
+})
+
+
+const View = Backbone.View.extend({
+    initialize: function () {
+        this.render()
+    },
+    htmlTemplate: _.template($('#item-template').html()),
+    render: function () {
+        return this.htmlTemplate(this.model)
+    }
+})
+
+// const myView = new View({
+//     model: { title: "hehe", text: 'hahaha' }
+// })
+
+
+const Views = Backbone.View.extend({
+    initialize: function () {
+        this.render()
+    },
+    // id: 'textInfo', // todo 为什么不能使用 this.$el 获取节点渲染到页面
+    htmlObj: $('#textInfo'),
+    render: function () {
+        let model = this.model
+        _.each(model, m => {
+            let m_view = new View({ model: m })
+            // this.$el.html(m_view.render().$el)
+            this.htmlObj.append(m_view.render())
+        })
+        console.log('this.$el: ', this.$el);
+        return this
+    }
+})
+
+const vs = new Views({
+    model: [
+        { title: "状态", text: '在售' },
+        { title: "域名", text: window.location.hostname },
+        { title: "邮箱", text: '346243440@qq.com' }
+    ]
+})
+// vs.render()
