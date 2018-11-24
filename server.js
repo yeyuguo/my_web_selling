db16ab6dfe15173c0d3a2244685860903f33952e
yeyuguo <346243440@qq.com>
2018-11-25 01:39:52 +0800

    feat(添加 80 和 443):

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express = require('express');
 var path = require('path');
 var fs = require('fs')
 var https = require('https')
+var http = require('http')
 var app = express();
+
+var http_app = http.Server(app);
 
 app.use(express.static('./'));
 
@@ -12,4 +15,5 @@ var certificate = fs.readFileSync(path.resolve('./https/server.crt'), 'utf8');
 var credentials = { key: privateKey, cert: certificate };
 
 var httpsServer = https.createServer(credentials, app);
-httpsServer.listen(8082)
+httpsServer.listen(443)
+http_app.listen(80);
