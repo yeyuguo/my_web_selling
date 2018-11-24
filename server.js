793cc8b7117486f98d990ce52f2f631a056574f9
yeyuguo <346243440@qq.com>
2018-11-25 01:23:18 +0800

    feat(添加 https 功能):

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs')
+var https = require('https')
+var app = express();
+
+app.use(express.static('./'));
+
+// 从 pms 路径下开始找的
+var privateKey = fs.readFileSync(path.resolve('./https/server.key'), 'utf8');
+var certificate = fs.readFileSync(path.resolve('./https/server.crt'), 'utf8');
+var credentials = { key: privateKey, cert: certificate };
+
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8082)
